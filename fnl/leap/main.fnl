ggandor/leap.nvim

commit b41f48643b483bb0881c0f7804f6f0be7bb95155

refactor(leap): improve `get-target-with-active-label`

fnl/leap/main.fnl
@@ -366,14 +366,16 @@ Also sets a `group` attribute (a static one too, not to be updated)."
                       n (min (+ (dec start) n) (length targets))))]
           (values start end))))
 
-  (fn get-target-with-active-label [sublist input]
-    (var res [])
-    (each [idx {: label : group &as target} (ipairs sublist)
-           &until (or (next res)
-                      (> (- (or group 0) _state.group-offset) 1))]
-      (when (and (= label input)
-                 (= (- group _state.group-offset) 1))
-        (set res [idx target])))
+  (fn get-target-with-active-label [targets input]
+    (var res nil)
+    (var break? false)
+    (each [idx target (ipairs targets) &until (or res break?)]
+      (when target.label
+        (local relative-group (- target.group _state.group-offset))
+        (when (> relative-group 1)  ; we are beyond the currently active group
+          (set break? true))
+        (when (and (= relative-group 1) (= target.label input))
+          (set res (values idx target)))))
     res)
 
   ; Getting targets
@@ -575,7 +577,7 @@ Also sets a `group` attribute (a static one too, not to be updated)."
                         (loop new-idx false))
                 ; We still want the labels (if there are) to function.
               _ (case (get-target-with-active-label targets in)
-                  [_ target] (jump-to! target)
+                  (_ target) (jump-to! target)
                   _ (vim.fn.feedkeys in :i))))))
 
     (loop start-idx true))
@@ -716,7 +718,7 @@ Also sets a `group` attribute (a static one too, not to be updated)."
             (exit-with-action-on 1)
             (do (vim.fn.feedkeys in-final :i) (exit)))))
 
-  (local [idx _] (get-target-with-active-label targets* in-final))
+  (local (idx _) (get-target-with-active-label targets* in-final))
   (if idx
       (exit-with-action-on idx)
       (do (vim.fn.feedkeys in-final :i) (exit)))

lua/leap/main.lua
@@ -319,16 +319,21 @@ local function leap(kwargs)
       return start, _end
     end
   end
-  local function get_target_with_active_label(sublist, input)
-    local res = {}
-    for idx, _55_ in ipairs(sublist) do
-      local _each_56_ = _55_
-      local label = _each_56_["label"]
-      local group = _each_56_["group"]
-      local target = _each_56_
-      if (next(res) or (((group or 0) - _state["group-offset"]) > 1)) then break end
-      if ((label == input) and ((group - _state["group-offset"]) == 1)) then
-        res = {idx, target}
+  local function get_target_with_active_label(targets, input)
+    local res = nil
+    local break_3f = false
+    for idx, target in ipairs(targets) do
+      if (res or break_3f) then break end
+      if target.label then
+        local relative_group = (target.group - _state["group-offset"])
+        if (relative_group > 1) then
+          break_3f = true
+        else
+        end
+        if ((relative_group == 1) and (target.label == input)) then
+          res = idx, target
+        else
+        end
       else
       end
     end
@@ -609,10 +614,10 @@ local function leap(kwargs)
             return loop(new_idx, false)
           else
             local _ = _97_
-            local _98_ = get_target_with_active_label(targets, _in)
-            if ((_G.type(_98_) == "table") and true and (nil ~= _98_[2])) then
-              local _0 = _98_[1]
-              local target = _98_[2]
+            local _98_, _99_ = get_target_with_active_label(targets, _in)
+            if (true and (nil ~= _99_)) then
+              local _0 = _98_
+              local target = _99_
               return jump_to_21(target)
             else
               local _0 = _98_
@@ -681,15 +686,15 @@ local function leap(kwargs)
   else
   end
   if dot_repeat_3f then
-    local _112_ = targets[state.dot_repeat.target_idx]
-    if (nil ~= _112_) then
-      local target = _112_
+    local _113_ = targets[state.dot_repeat.target_idx]
+    if (nil ~= _113_) then
+      local target = _113_
       do_action(target)
       hl:cleanup(hl_affected_windows)
       exec_user_autocmds("LeapLeave")
       return
     else
-      local _ = _112_
+      local _ = _113_
       if change_op_3f then
         handle_interrupted_change_op_21()
       else
@@ -874,9 +879,7 @@ local function leap(kwargs)
     end
   else
   end
-  local _local_146_ = get_target_with_active_label(targets_2a, in_final)
-  local idx = _local_146_[1]
-  local _ = _local_146_[2]
+  local idx, _ = get_target_with_active_label(targets_2a, in_final)
   if idx then
     set_dot_repeat(in1, _3fin20, idx)
     do_action(targets_2a[idx])

commit cf8616b16d5cd9f8adf695318116e93db5818cec

fix(leap): labels should work in traversal mode
Regression introduced by b41f486: `get-target-with-active-label` never
returned the target itself (the assignment to the single variable `res`
swallowed it).

fnl/leap/main.fnl
@@ -381,7 +381,7 @@ Also sets a `group` attribute (a static one too, not to be updated)."
         (when (> relative-group 1)  ; we are beyond the currently active group
           (set break? true))
         (when (and (= relative-group 1) (= target.label input))
-          (set res (values idx target)))))
+          (set res [idx target]))))
     res)
 
   ; Getting targets
@@ -587,7 +587,7 @@ Also sets a `group` attribute (a static one too, not to be updated)."
                         (loop new-idx false))
                 ; We still want the labels (if there are) to function.
               _ (case (get-target-with-active-label targets in)
-                  (_ target) (jump-to! target)
+                  [_ target] (jump-to! target)
                   _ (vim.fn.feedkeys in :i))))))
 
     (loop start-idx true))
@@ -724,7 +724,7 @@ Also sets a `group` attribute (a static one too, not to be updated)."
             (exit-with-action-on 1)
             (do (vim.fn.feedkeys in-final :i) (exit)))))
 
-  (local (idx _) (get-target-with-active-label targets* in-final))
+  (local [idx _] (get-target-with-active-label targets* in-final))
   (if idx
       (exit-with-action-on idx)
       (do (vim.fn.feedkeys in-final :i) (exit)))

lua/leap/main.lua
@@ -331,7 +331,7 @@ local function leap(kwargs)
         else
         end
         if ((relative_group == 1) and (target.label == input)) then
-          res = idx, target
+          res = {idx, target}
         else
         end
       else
@@ -615,10 +615,10 @@ local function leap(kwargs)
             return loop(new_idx, false)
           else
             local _ = _97_
-            local _98_, _99_ = get_target_with_active_label(targets, _in)
-            if (true and (nil ~= _99_)) then
-              local _0 = _98_
-              local target = _99_
+            local _98_ = get_target_with_active_label(targets, _in)
+            if ((_G.type(_98_) == "table") and true and (nil ~= _98_[2])) then
+              local _0 = _98_[1]
+              local target = _98_[2]
               return jump_to_21(target)
             else
               local _0 = _98_
@@ -687,15 +687,15 @@ local function leap(kwargs)
   else
   end
   if dot_repeating_3f then
-    local _113_ = targets[state.dot_repeat.target_idx]
-    if (nil ~= _113_) then
-      local target = _113_
+    local _112_ = targets[state.dot_repeat.target_idx]
+    if (nil ~= _112_) then
+      local target = _112_
       do_action(target)
       hl:cleanup(hl_affected_windows)
       exec_user_autocmds("LeapLeave")
       return
     else
-      local _ = _113_
+      local _ = _112_
       if change_op_3f then
         handle_interrupted_change_op_21()
       else
@@ -880,7 +880,9 @@ local function leap(kwargs)
     end
   else
   end
-  local idx, _ = get_target_with_active_label(targets_2a, in_final)
+  local _local_146_ = get_target_with_active_label(targets_2a, in_final)
+  local idx = _local_146_[1]
+  local _ = _local_146_[2]
   if idx then
     set_dot_repeat(in1, _3fin20, idx)
     do_action(targets_2a[idx])