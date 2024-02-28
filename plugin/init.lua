3a3d210cf69872cf7192ac45ee0cb620d65a06c1
pamacs <gyorgy.andorka@protonmail.com>
2024-02-28 22:25:01 +0100

    style(plugin/init): readability tweaks

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,58 +1,80 @@
 local plug_mappings = {
+  -- Default
   {
-    {'n', 'x', 'o'}, '<Plug>(leap-forward)',
+    { 'n', 'x', 'o' },
+    '<Plug>(leap-forward)',
     function () require('leap').leap {} end
   },
   {
-    {'n', 'x', 'o'}, '<Plug>(leap-backward)',
+    { 'n', 'x', 'o' },
+    '<Plug>(leap-backward)',
     function () require('leap').leap { backward = true } end
   },
   {
-    {'n'}, '<Plug>(leap-forward-to)',
+    { 'n', 'x', 'o' },
+    '<Plug>(leap-from-window)',
+    function ()
+      require('leap').leap {
+        target_windows = require('leap.util').get_enterable_windows()
+      }
+    end
+  },
+
+  -- Alternative
+  {
+    { 'n' },
+    '<Plug>(leap-forward-to)',
     function () require('leap').leap {} end
   },
   {
-    {'x', 'o'}, '<Plug>(leap-forward-to)',
-    function () require('leap').leap {
+    { 'x', 'o' },
+    '<Plug>(leap-forward-to)',
+    function ()
+      require('leap').leap {
         offset = 1, inclusive_op = true, match_same_char_seq_at_end = true
       }
     end
   },
   {
-    {'n', 'x', 'o'}, '<Plug>(leap-forward-till)',
+    { 'n', 'x', 'o' },
+    '<Plug>(leap-forward-till)',
     function () require('leap').leap { offset = -1, inclusive_op = true } end
   },
   {
-    {'n', 'x', 'o'}, '<Plug>(leap-backward-to)',
+    { 'n', 'x', 'o' },
+    '<Plug>(leap-backward-to)',
+    function () require('leap').leap { backward = true } end
+  },
+  {
+    { 'n', 'x', 'o' },
+    '<Plug>(leap-backward-till)',
     function ()
-      require('leap').leap { backward = true, }
-    end
-  },
-  {
-    {'n', 'x', 'o'}, '<Plug>(leap-backward-till)',
-    function () require('leap').leap {
-        backward = true, offset = 2, match_same_char_seq_at_end = true,
-      }
-    end
-  },
-  {
-    {'n', 'x', 'o'}, '<Plug>(leap-from-window)',
-    function () require('leap').leap {
-        target_windows = require'leap.util'.get_enterable_windows()
+      require('leap').leap {
+        backward = true, offset = 2, match_same_char_seq_at_end = true
       }
     end
   },
 
-  -- Deprecated mappings.
+  -- Deprecated
   {
-    {'n', 'x', 'o'}, '<Plug>(leap-cross-window)',
-    function () require('leap').leap { target_windows = require'leap.util'.get_enterable_windows() } end
+    { 'n', 'x', 'o' },
+    '<Plug>(leap-cross-window)',
+    '<Plug>(leap-from-window)'
   },
-  {{'n', 'x', 'o'}, '<Plug>(leap-forward-x)',  function () require('leap').leap { offset = 1, inclusive_op = true } end},
-  {{'n', 'x', 'o'}, '<Plug>(leap-backward-x)', function () require('leap').leap { backward = true, offset = 2 } end},
+  {
+    { 'n', 'x', 'o' },
+    '<Plug>(leap-forward-x)',
+    function () require('leap').leap { offset = 1, inclusive_op = true } end
+  },
+  {
+    { 'n', 'x', 'o' },
+    '<Plug>(leap-backward-x)',
+    function () require('leap').leap { backward = true, offset = 2 } end
+  },
 }
+
 
 for _, t in ipairs(plug_mappings) do
   local modes, lhs, rhs = unpack(t)
-  vim.keymap.set(modes, lhs, rhs, {silent = true})
+  vim.keymap.set(modes, lhs, rhs, { silent = true })
 end
