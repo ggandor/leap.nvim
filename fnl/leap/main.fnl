ggandor/leap.nvim

commit ce65ca3563647a73c7e5555e0c94eed1317d2d83

improv(api): deprecate support for label lists as tables

fnl/leap/main.fnl
@@ -19,7 +19,6 @@
        (require "leap.util"))
 
 (local api vim.api)
-(local empty? vim.tbl_isempty)
 (local {: abs : ceil : floor : min} math)
 
 
@@ -159,14 +158,14 @@ char separately.
         (and (= l1 l2) (= c1 (+ c2 (char1:len) (char2:len))))))
 
     (fn set-autojump [targets]
-      (when-not (empty? opts.safe_labels)
+      (when (not= opts.safe_labels "")
         (set targets.autojump?
-             (or (empty? opts.labels)                                       ; forced
+             (or (= opts.labels "")                                         ; forced
                  (>= (length opts.safe_labels) (dec (length targets)))))))  ; smart
 
     (fn attach-label-set [targets]
-      (set targets.label-set (if (empty? opts.labels) opts.safe_labels
-                                 (empty? opts.safe_labels) opts.labels
+      (set targets.label-set (if (= opts.labels "") opts.safe_labels
+                                 (= opts.safe_labels "") opts.labels
                                  targets.autojump? opts.safe_labels
                                  opts.labels)))
 
@@ -180,10 +179,10 @@ char separately.
           (when (>= i 1)
             (case (% i |label-set|)
               0 (do
-                  (set target.label (. label-set |label-set|))
+                  (set target.label (label-set:sub |label-set| |label-set|))
                   (set target.group (floor (/ i |label-set|))))
               n (do
-                  (set target.label (. label-set n))
+                  (set target.label (label-set:sub n n))
                   (set target.group (inc (floor (/ i |label-set|))))))))))
 
     (fn [targets force-noautojump? multi-window-search?]
@@ -294,20 +293,22 @@ char separately.
             ; Prevent merging with the defaults, as this is derived
             ; programmatically from a list-like option (see opts.fnl).
             (setmetatable {:merge false})))
-  ; Force the label lists into tables.
+
+  ; Force the label lists into strings (table support is deprecated).
   (each [_ t (ipairs [:default :current_call])]
     (each [_ k (ipairs [:labels :safe_labels])]
-      (when (= (type (. opts t k)) :string)
-        (set (. opts t k) (vim.fn.split (. opts t k) "\\zs")))))
+      (when (= (type (. opts t k)) :table)
+        (set (. opts t k)
+             (table.concat (. opts t k))))))
 
   (local directional? (not target-windows))
-  (local no-labels-to-use? (and (empty? opts.labels)
-                                (empty? opts.safe_labels)))
+  (local no-labels-to-use? (and (= opts.labels "")
+                                (= opts.safe_labels "")))
 
   (when (and (not directional?) no-labels-to-use?)
     (echo "no labels to use")
     (lua :return))
-  (when (and target-windows (empty? target-windows))
+  (when (and target-windows (vim.tbl_isempty target-windows))
     (echo "no targetable windows")
     (lua :return))
 
@@ -694,7 +695,7 @@ char separately.
             (set t.label nil))
 
           ; Remove all the subsequent label groups if needed.
-          (not (empty? opts.safe_labels))
+          (not= opts.safe_labels "")
           (let [last-labeled (inc (length opts.safe_labels))]  ; skipped the first
             (for [i (inc last-labeled) (length targets)]
               (set (. targets i :label) nil)

lua/leap/main.lua
@@ -16,7 +16,6 @@ local get_representative_char = _local_2_["get-representative-char"]
 local get_char = _local_2_["get-char"]
 local get_char_keymapped = _local_2_["get-char-keymapped"]
 local api = vim.api
-local empty_3f = vim.tbl_isempty
 local abs = math["abs"]
 local ceil = math["ceil"]
 local floor = math["floor"]
@@ -108,17 +107,17 @@ do
     end
   end
   local function set_autojump(targets)
-    if not empty_3f(opts.safe_labels) then
-      targets["autojump?"] = (empty_3f(opts.labels) or (#opts.safe_labels >= dec(#targets)))
+    if (opts.safe_labels ~= "") then
+      targets["autojump?"] = ((opts.labels == "") or (#opts.safe_labels >= dec(#targets)))
       return nil
     else
       return nil
     end
   end
   local function attach_label_set(targets)
-    if empty_3f(opts.labels) then
+    if (opts.labels == "") then
       targets["label-set"] = opts.safe_labels
-    elseif empty_3f(opts.safe_labels) then
+    elseif (opts.safe_labels == "") then
       targets["label-set"] = opts.labels
     elseif targets["autojump?"] then
       targets["label-set"] = opts.safe_labels
@@ -142,11 +141,11 @@ do
         if (i >= 1) then
           local _17_ = (i % _7clabel_set_7c)
           if (_17_ == 0) then
-            target.label = label_set[_7clabel_set_7c]
+            target.label = label_set:sub(_7clabel_set_7c, _7clabel_set_7c)
             target.group = floor((i / _7clabel_set_7c))
           elseif (nil ~= _17_) then
             local n = _17_
-            target.label = label_set[n]
+            target.label = label_set:sub(n, n)
             target.group = inc(floor((i / _7clabel_set_7c)))
           else
           end
@@ -229,20 +228,20 @@ local function leap(kwargs)
   end
   for _, t in ipairs({"default", "current_call"}) do
     for _0, k in ipairs({"labels", "safe_labels"}) do
-      if (type(opts[t][k]) == "string") then
-        opts[t][k] = vim.fn.split(opts[t][k], "\\zs")
+      if (type(opts[t][k]) == "table") then
+        opts[t][k] = table.concat(opts[t][k])
       else
       end
     end
   end
   local directional_3f = not target_windows
-  local no_labels_to_use_3f = (empty_3f(opts.labels) and empty_3f(opts.safe_labels))
+  local no_labels_to_use_3f = ((opts.labels == "") and (opts.safe_labels == ""))
   if (not directional_3f and no_labels_to_use_3f) then
     echo("no labels to use")
     return
   else
   end
-  if (target_windows and empty_3f(target_windows)) then
+  if (target_windows and vim.tbl_isempty(target_windows)) then
     echo("no targetable windows")
     return
   else
@@ -680,7 +679,7 @@ local function leap(kwargs)
           t.label = nil
         end
         return nil
-      elseif not empty_3f(opts.safe_labels) then
+      elseif (opts.safe_labels ~= "") then
         local last_labeled = inc(#opts.safe_labels)
         for i = inc(last_labeled), #targets do
           targets[i]["label"] = nil

fnl/leap/opts.fnl
@@ -1,14 +1,9 @@
 (local M {:default {:case_sensitive false
                     :equivalence_classes [" \t\r\n"]
                     :preview_filter nil
-                    :safe_labels ["s" "f" "n" "u" "t" "/"
-                                  "S" "F" "N" "L" "H" "M" "U" "G" "T" "Z" "?"]
-                    :labels ["s" "f" "n"
-                             "j" "k" "l" "h" "o" "d" "w" "e" "i" "m" "b" "u"
-                             "y" "v" "r" "g" "t" "a" "q" "p" "c" "x" "z" "/"
-                             "S" "F" "N"
-                             "J" "K" "L" "H" "O" "D" "W" "E" "I" "M" "B" "U"
-                             "Y" "V" "R" "G" "T" "A" "Q" "P" "C" "X" "Z" "?"]
+                    :safe_labels "sfnut/SFNLHMUGTZ?"
+                    :labels (.. "sfnjklhodweimbuyvrgtaqpcxz/"
+                                "SFNJKLHODWEIMBUYVRGTAQPCXZ?")
                     :keys {:next_target "<enter>"
                            :prev_target "<backspace>"
                            :next_group "<space>"

fnl/leap/user.fnl
@@ -5,29 +5,27 @@
 `keys.next_target`, `keys.prev_target`, and `safe_labels` set
 appropriately."
   (local leap (require "leap"))
-  (local keys (vim.deepcopy leap.opts.keys))
 
   (fn with-key [t key]
     (local t (case (type t) :table t _ [t]))
     (table.insert t key)
     t)
 
+  (local keys (vim.deepcopy leap.opts.keys))
   (local keys* {:next_target (with-key keys.next_target fwd-key)
                 :prev_target (with-key keys.prev_target bwd-key)})
 
   ; Remove the "prev" key from `safe_labels`, and use the "next" key as
   ; the first label.
+  ; NOTE: support for tables is deprecated.
   (var safe-labels (vim.deepcopy leap.opts.safe_labels))
-  (when (= (type safe-labels) :string)
-    (set safe-labels (vim.fn.split safe-labels "\\zs")))
-
-  (local safe-labels*
-         (icollect [_ l (ipairs safe-labels) :into [fwd-key]]
-           (when (and (not= l fwd-key) (not= l bwd-key))
-             l)))
+  (when (= (type safe-labels) :table)
+    (set safe-labels (table.concat safe-labels)))
+  (set safe-labels (.. fwd-key
+                       (safe-labels:gsub (.. "[" fwd-key bwd-key "]") "")))
 
   {:keys keys*
-   :safe_labels safe-labels*})
+   :safe_labels safe-labels})
 
 
 (fn set-repeat-keys [fwd-key bwd-key opts*]

lua/leap/opts.lua
@@ -1,6 +1,6 @@
 -- Code generated from fnl/leap/opts.fnl - do not edit directly.
 
-local M = {default = {equivalence_classes = {" \9\13\n"}, preview_filter = nil, safe_labels = {"s", "f", "n", "u", "t", "/", "S", "F", "N", "L", "H", "M", "U", "G", "T", "Z", "?"}, labels = {"s", "f", "n", "j", "k", "l", "h", "o", "d", "w", "e", "i", "m", "b", "u", "y", "v", "r", "g", "t", "a", "q", "p", "c", "x", "z", "/", "S", "F", "N", "J", "K", "L", "H", "O", "D", "W", "E", "I", "M", "B", "U", "Y", "V", "R", "G", "T", "A", "Q", "P", "C", "X", "Z", "?"}, keys = {next_target = "<enter>", prev_target = "<backspace>", next_group = "<space>", prev_group = "<backspace>"}, vim_opts = {["wo.scrolloff"] = 0, ["wo.sidescrolloff"] = 0, ["wo.conceallevel"] = 0, ["bo.modeline"] = false}, max_highlighted_traversal_targets = 10, substitute_chars = {}, case_sensitive = false, highlight_unlabeled_phase_one_targets = false}, current_call = {}}
+local M = {default = {equivalence_classes = {" \9\13\n"}, preview_filter = nil, safe_labels = "sfnut/SFNLHMUGTZ?", labels = ("sfnjklhodweimbuyvrgtaqpcxz/" .. "SFNJKLHODWEIMBUYVRGTAQPCXZ?"), keys = {next_target = "<enter>", prev_target = "<backspace>", next_group = "<space>", prev_group = "<backspace>"}, vim_opts = {["wo.scrolloff"] = 0, ["wo.sidescrolloff"] = 0, ["wo.conceallevel"] = 0, ["bo.modeline"] = false}, max_highlighted_traversal_targets = 10, substitute_chars = {}, case_sensitive = false, highlight_unlabeled_phase_one_targets = false}, current_call = {}}
 local function _1_(self, key_2a)
   local key
   if (key_2a == "special_keys") then

commit 44e3a60a7a62069e5c9445b7a8104612d4f060cb

fix(main): unicode labels
Regression introduced by ce65ca3563647a73c7e5555e0c94eed1317d2d83.

Closes #277

fnl/leap/main.fnl
@@ -204,29 +204,34 @@ char.
 
 
 ; Does _not_ mutate its argument.
-(fn get-traversable-labels [labels]
-  (local ks opts.keys)
-  (var bad-keys "")
-  (each [_ key (ipairs [ks.next_target ks.prev_target])]
-    (set bad-keys (.. bad-keys
-                      (if (= (type key) :table)
-                          (table.concat (vim.tbl_map vim.keycode key))
-                          (vim.keycode key)))))
-  (local sanitized (labels:gsub (.. "[" bad-keys "]") ""))
-  (local nxt (and (= (type ks.next_target) :table)
-                  (. ks.next_target 2)))
-  (if (and nxt (= (vim.keycode nxt) nxt))
-      (.. nxt sanitized)
-      sanitized))
+(fn as-traversable [labels]
+  (if (= (length labels) 0) labels
+      (do
+        (local ks opts.keys)
+        (var bad-keys "")
+        (each [_ key (ipairs [ks.next_target ks.prev_target])]
+          (set bad-keys (.. bad-keys
+                            (if (= (type key) :table)
+                                (table.concat (vim.tbl_map vim.keycode key))
+                                (vim.keycode key)))))
+        (local sanitized (labels:gsub (.. "[" bad-keys "]") ""))
+        (local next-key (and (= (type ks.next_target) :table)
+                             (. ks.next_target 2)))
+        (if (and next-key (= (vim.keycode next-key) next-key))
+            (.. next-key sanitized)
+            sanitized))))
 
 
-(fn prepare-labeled-targets [targets can-traverse? force-noautojump? multi-window?]
-  (local [labels safe-labels]
-         (if can-traverse? [(if (= opts.labels "") opts.labels
-                                (get-traversable-labels opts.labels))
-                            (if (= opts.safe_labels "") opts.safe_labels
-                                (get-traversable-labels opts.safe_labels))]
-                           [opts.labels opts.safe_labels]))
+(fn prepare-labeled-targets [targets kwargs]
+  (local {: can-traverse? : force-noautojump? : multi-window?} kwargs)
+  (local [labels safe-labels] (if can-traverse?
+                                  (vim.tbl_map as-traversable
+                                               [opts.labels opts.safe_labels])
+                                  [opts.labels opts.safe_labels]))
+  ; Strings are handy as API and for manipulations, but from this point
+  ; on we want efficient access. (Use vim.fn.split for unicode.)
+  (local [labels safe-labels] (vim.tbl_map #(vim.fn.split $ "\\zs")
+                                           [labels safe-labels]))
 
   ; Problem:
   ; We are autojumping to some position in window A, but our chosen
@@ -272,17 +277,17 @@ char.
   (fn set-autojump [targets]
     "Set a flag indicating whether we can automatically jump to the
     first target, without having to select a label."
-    (when (not= safe-labels "")
+    (when (> (length safe-labels) 0)
       (set targets.autojump?
-           (or (= labels "")                      ; forced
+           (or (= (length labels) 0)              ; forced
                (enough-safe-labels? targets)))))  ; smart
 
   (fn attach-label-set [targets]
     ; Note that there is no one-to-one correspondence between the
     ; `autojump?` flag and this field. No-autojump might be forced
     ; implicitly, regardless of using safe labels.
-    (set targets.label-set (if (= labels "") safe-labels
-                               (= safe-labels "") labels
+    (set targets.label-set (if (= (length labels) 0) safe-labels
+                               (= (length safe-labels) 0) labels
                                targets.autojump? safe-labels
                                labels)))
 
@@ -302,10 +307,10 @@ char.
         (if target.offscreen? (set skipped (inc skipped))
           (case (% i* |labels|)
             0 (do
-                (set target.label (labels:sub |labels| |labels|))
+                (set target.label (. labels |labels|))
                 (set target.group (floor (/ i* |labels|))))
             n (do
-                (set target.label (labels:sub n n))
+                (set target.label (. labels n))
                 (set target.group (inc (floor (/ i* |labels|))))))))))
 
   (when-not (or force-noautojump?
@@ -428,8 +433,8 @@ char.
              (table.concat (. opts t k))))))
 
   (local directional? (not windows))
-  (local no-labels-to-use? (and (= opts.labels "")
-                                (= opts.safe_labels "")))
+  (local no-labels-to-use? (and (= (length opts.labels) 0)
+                                (= (length opts.safe_labels) 0)))
 
   (when (and (not directional?) no-labels-to-use?)
     (echo "no labels to use")
@@ -642,9 +647,9 @@ char.
                                    (and op-mode? (> (length targets) 1))))
           multi-window? (and windows (> (length windows) 1))]
       (prepare-labeled-targets targets
-                               (can-traverse? targets)
-                               force-noautojump?
-                               multi-window?)))
+                               {:can-traverse? (can-traverse? targets)
+                                : force-noautojump?
+                                : multi-window?})))
 
   ; Repeat
 

lua/leap/main.lua
@@ -198,50 +198,50 @@ local function populate_sublists(targets)
   end
   return nil
 end
-local function get_traversable_labels(labels)
-  local ks = opts.keys
-  local bad_keys = ""
-  for _, key in ipairs({ks.next_target, ks.prev_target}) do
-    local _33_
-    if (type(key) == "table") then
-      _33_ = table.concat(vim.tbl_map(vim.keycode, key))
-    else
-      _33_ = vim.keycode(key)
-    end
-    bad_keys = (bad_keys .. _33_)
-  end
-  local sanitized = labels:gsub(("[" .. bad_keys .. "]"), "")
-  local nxt = ((type(ks.next_target) == "table") and ks.next_target[2])
-  if (nxt and (vim.keycode(nxt) == nxt)) then
-    return (nxt .. sanitized)
+local function as_traversable(labels)
+  if (#labels == 0) then
+    return labels
   else
-    return sanitized
+    local ks = opts.keys
+    local bad_keys = ""
+    for _, key in ipairs({ks.next_target, ks.prev_target}) do
+      local _33_
+      if (type(key) == "table") then
+        _33_ = table.concat(vim.tbl_map(vim.keycode, key))
+      else
+        _33_ = vim.keycode(key)
+      end
+      bad_keys = (bad_keys .. _33_)
+    end
+    local sanitized = labels:gsub(("[" .. bad_keys .. "]"), "")
+    local next_key = ((type(ks.next_target) == "table") and ks.next_target[2])
+    if (next_key and (vim.keycode(next_key) == next_key)) then
+      return (next_key .. sanitized)
+    else
+      return sanitized
+    end
   end
 end
-local function prepare_labeled_targets(targets, can_traverse_3f, force_noautojump_3f, multi_window_3f)
-  local function _39_()
+local function prepare_labeled_targets(targets, kwargs)
+  local can_traverse_3f = kwargs["can-traverse?"]
+  local force_noautojump_3f = kwargs["force-noautojump?"]
+  local multi_window_3f = kwargs["multi-window?"]
+  local function _37_()
     if can_traverse_3f then
-      local _36_
-      if (opts.labels == "") then
-        _36_ = opts.labels
-      else
-        _36_ = get_traversable_labels(opts.labels)
-      end
-      local function _38_()
-        if (opts.safe_labels == "") then
-          return opts.safe_labels
-        else
-          return get_traversable_labels(opts.safe_labels)
-        end
-      end
-      return {_36_, _38_()}
+      return vim.tbl_map(as_traversable, {opts.labels, opts.safe_labels})
     else
       return {opts.labels, opts.safe_labels}
     end
   end
-  local _local_40_ = _39_()
-  local labels = _local_40_[1]
-  local safe_labels = _local_40_[2]
+  local _local_38_ = _37_()
+  local labels = _local_38_[1]
+  local safe_labels = _local_38_[2]
+  local function _39_(_241)
+    return vim.fn.split(_241, "\\zs")
+  end
+  local _local_40_ = vim.tbl_map(_39_, {labels, safe_labels})
+  local labels0 = _local_40_[1]
+  local safe_labels0 = _local_40_[2]
   local function all_in_the_same_window_3f(targets0)
     local same_win_3f = true
     local win = targets0[1].wininfo.winid
@@ -268,7 +268,7 @@ local function prepare_labeled_targets(targets, can_traverse_3f, force_noautojum
     end
   end
   local function enough_safe_labels_3f(targets0)
-    local limit = (#safe_labels + 1)
+    local limit = (#safe_labels0 + 1)
     local count = 0
     for _, t in ipairs(targets0) do
       if (count > limit) then break end
@@ -280,29 +280,29 @@ local function prepare_labeled_targets(targets, can_traverse_3f, force_noautojum
     return (count <= limit)
   end
   local function set_autojump(targets0)
-    if (safe_labels ~= "") then
-      targets0["autojump?"] = ((labels == "") or enough_safe_labels_3f(targets0))
+    if (#safe_labels0 > 0) then
+      targets0["autojump?"] = ((#labels0 == 0) or enough_safe_labels_3f(targets0))
       return nil
     else
       return nil
     end
   end
   local function attach_label_set(targets0)
-    if (labels == "") then
-      targets0["label-set"] = safe_labels
-    elseif (safe_labels == "") then
-      targets0["label-set"] = labels
+    if (#labels0 == 0) then
+      targets0["label-set"] = safe_labels0
+    elseif (#safe_labels0 == 0) then
+      targets0["label-set"] = labels0
     elseif targets0["autojump?"] then
-      targets0["label-set"] = safe_labels
+      targets0["label-set"] = safe_labels0
     else
-      targets0["label-set"] = labels
+      targets0["label-set"] = labels0
     end
     return nil
   end
   local function set_labels(targets0)
     local autojump_3f = targets0["autojump?"]
-    local labels0 = targets0["label-set"]
-    local _7clabels_7c = #labels0
+    local labels1 = targets0["label-set"]
+    local _7clabels_7c = #labels1
     local skipped
     if autojump_3f then
       skipped = 1
@@ -324,11 +324,11 @@ local function prepare_labeled_targets(targets, can_traverse_3f, force_noautojum
         else
           local _49_ = (i_2a % _7clabels_7c)
           if (_49_ == 0) then
-            target.label = labels0:sub(_7clabels_7c, _7clabels_7c)
+            target.label = labels1[_7clabels_7c]
             target.group = floor((i_2a / _7clabels_7c))
           elseif (nil ~= _49_) then
             local n = _49_
-            target.label = labels0:sub(n, n)
+            target.label = labels1[n]
             target.group = (floor((i_2a / _7clabels_7c)) + 1)
           else
           end
@@ -462,7 +462,7 @@ local function leap(kwargs)
     end
   end
   local directional_3f = not windows
-  local no_labels_to_use_3f = ((opts.labels == "") and (opts.safe_labels == ""))
+  local no_labels_to_use_3f = ((#opts.labels == 0) and (#opts.safe_labels == 0))
   if (not directional_3f and no_labels_to_use_3f) then
     echo("no labels to use")
     return
@@ -730,7 +730,7 @@ local function leap(kwargs)
   local function prepare_labeled_targets_2a(targets)
     local force_noautojump_3f = (not action_can_traverse_3f and (user_given_action or (op_mode_3f and (#targets > 1))))
     local multi_window_3f = (windows and (#windows > 1))
-    return prepare_labeled_targets(targets, can_traverse_3f(targets), force_noautojump_3f, multi_window_3f)
+    return prepare_labeled_targets(targets, {["can-traverse?"] = can_traverse_3f(targets), ["force-noautojump?"] = force_noautojump_3f, ["multi-window?"] = multi_window_3f})
   end
   local repeat_state = {offset = kwargs.offset, backward = kwargs.backward, inclusive = kwargs.inclusive, pattern = kwargs.pattern, inputlen = inputlen0, opts = opts_current_call}
   local function update_repeat_state(in1, in2)